bee1c9d4429888eaa5a77c607313502b208ac2d3
abrudana_cp <SND\abrudana_cp>
2014-08-27 20:33:35 +0000

    AA

---
+++ WorldWideAstronomy/WWATest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWATest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
