9f8cdbabf329e00fc028abe923a0e880e9d86660
abrudana_cp <SND\abrudana_cp>
2014-08-29 20:06:08 +0000

    AA

--- WorldWideAstronomy/WWA/Properties/AssemblyInfo.cs
+++ WorldWideAstronomy/WWA/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WWA")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WorldWideAstronomy")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Attila Abrudan")]
 [assembly: AssemblyProduct("WWA")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
