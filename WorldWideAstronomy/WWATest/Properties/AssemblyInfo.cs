7e8981e077aa41a03d2c13debdad00ce5a605696
Attila Abrudán <abrudana@outlook.com>
2019-02-28 20:51:35 +0100

    AA

--- WorldWideAstronomy/WWATest/Properties/AssemblyInfo.cs
+++ WorldWideAstronomy/WWATest/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WWATest")]
-[assembly: AssemblyDescription("WorldWideAstronomy")]
+[assembly: AssemblyDescription("WorldWideAstronomy - SOFA (Standards of Fundamental Astronomy)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Attila Abrudan")]
 [assembly: AssemblyProduct("WWATest")]
