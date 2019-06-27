d1298df004f895cf140bee9c7681dba4fe0c995b
abrudana <abrudana@gmail.com>
2019-06-27 16:20:04 +0200

    New .NET Core projects.

---
+++ WorldWideAstronomy/WWATest.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WWATest.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
